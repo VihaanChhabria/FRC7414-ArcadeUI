6c4292694b7624bf50859574d7db4479124e23f0
VihaanChhabria <chhabria.vihaan@gmail.com>
2025-03-31 12:47:32 -0400

    added sound effects

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+
+import SelectSound from "../assets/SelectSound.mp3";
+import EnterSound from "../assets/EnterSound.mp3";
 
 const MainLayout = () => {
   const [counter, setCounter] = useState(0);
   const location = useLocation();
   const [pageKey, setPageKey] = useState(location.pathname);
+
+  const audioSelectRef = useRef(null);
+  const audioEnterRef = useRef(null);
 
   useEffect(() => {
     setPageKey(location.pathname);
@@ -20,12 +26,29 @@ const MainLayout = () => {
         setCounter(
           (prev) => (prev - 1 + allSelectors.length) % allSelectors.length
         );
+        audioSelectRef.current.pause();
+        audioEnterRef.current.currentTime = 0;
+        audioEnterRef.current.pause();
+        audioEnterRef.current.currentTime = 0;
+        audioSelectRef.current.play();
       } else if (event.key === "ArrowRight" || event.key === "ArrowDown") {
         setCounter((prev) => (prev + 1) % allSelectors.length);
+        audioSelectRef.current.pause();
+        audioEnterRef.current.currentTime = 0;
+        audioEnterRef.current.pause();
+        audioEnterRef.current.currentTime = 0;
+        audioSelectRef.current.play();
       } else {
         const currentElement = document.querySelector(`.selector${counter}`);
         if (currentElement) {
           currentElement.click();
+          setTimeout(() => {
+            audioSelectRef.current.pause();
+            audioEnterRef.current.currentTime = 0;
+            audioEnterRef.current.pause();
+            audioEnterRef.current.currentTime = 0;
+            audioEnterRef.current.play();
+          }, 10); // 2-second delay
         }
       }
     };
@@ -34,7 +57,7 @@ const MainLayout = () => {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [counter]);
+  }, []);
 
   useEffect(() => {
     const allSelectors = document.querySelectorAll('[class^="selector"]');
@@ -48,7 +71,11 @@ const MainLayout = () => {
   }, [counter]);
 
   return (
-    <div key={pageKey} style={{ width: "100%", height: "100%" }} className={"scanlines"}>
+    <div
+      key={pageKey}
+      style={{ width: "100%", height: "100%" }}
+      className={"scanlines"}
+    >
       <style>
         {`
           @keyframes blink {
@@ -61,6 +88,8 @@ const MainLayout = () => {
           }
         `}
       </style>
+      <audio ref={audioSelectRef} src={SelectSound} />
+      <audio ref={audioEnterRef} src={EnterSound} />
       <Outlet />
     </div>
   );
